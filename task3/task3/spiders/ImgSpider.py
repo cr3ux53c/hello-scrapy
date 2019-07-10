2fd8be88dec1bb7f98491666cf585e78436b61fc
LIM HYUN SEOK <cr3ux53c@gmail.com>
2019-07-10 23:10:17 +0900

    리펙토링

--- task3/task3/spiders/ImgSpider.py
+++ task3/task3/spiders/ImgSpider.py
@@ -14,8 +14,6 @@ class ImgSpider(Spider):
         # 부모가 body인 모든 a 태그 중 속성 중 ..으로 시작하는 태그 추출
         # https://www.w3schools.com/cssref/css_selectors.asp
         for img in response.css("body > a[href^=\"../\"]"):
-            imgurl = img.css("a::attr(href)").extract_first()
-            imgurl = 'http://licenseplatemania.com' + imgurl[2:]  # 상대 경로 대신 절대 경로로 변환
-            img_urls.append(imgurl)
+            img_urls.append('http://licenseplatemania.com' + img.css("a::attr(href)").extract_first()[2:]) # 상대 경로 대신 절대 경로로 변환
         item["image_urls"] = img_urls
         return item
