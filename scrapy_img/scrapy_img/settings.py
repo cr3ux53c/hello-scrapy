1cce190b1e0b583cb7a825a768b98e4002c02607
LIM HYUN SEOK <cr3ux53c@gmail.com>
2019-07-10 22:08:41 +0900

    agent 강제

--- scrapy_img/scrapy_img/settings.py
+++ scrapy_img/scrapy_img/settings.py
@@ -15,10 +15,10 @@ SPIDER_MODULES = ['scrapy_img.spiders']
 NEWSPIDER_MODULE = 'scrapy_img.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-# USER_AGENT = 'scrapy_img (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 # CONCURRENT_REQUESTS = 32
